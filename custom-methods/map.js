4347eb47187e618d81461652db49b83ca5f1bb5e
SaulReznik <robhov97@gmail.com>
2020-12-08 19:26:56 +0400

    promise.map

--- custom-methods/map.js
+++ custom-methods/map.js
@@ -1,20 +1,41 @@
-const sample = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const bluebird = require('bluebird');
 
-const map = (arr, cb) => {
+const arr = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)];
+
+const map = (arr, callback, options = {}) => {
+  //set the default value of concurrency
+  //so it'll work exactly like regular Promise.all
+  //resolving all promises in queue
+  let concurrency = options.concurrency || Infinity;
+
+  let index = 0;
   const result = [];
+  const pendingPromisesArr = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    result.push(cb(arr[i], i, arr));
+  //resolving every promise in the queue
+  const wrappedCallback = () => {
+    if (index >= arr.length) return null;
+    const i = index++;
+
+    return callback(arr[i], i).then(resolved => {
+      result[i] = resolved;
+      return wrappedCallback();
+    });
+  };
+
+  //checking concurency to not iterate more then remaining elements in arr
+  while (concurrency-- > 0) {
+    const pendingPromise = wrappedCallback();
+    if (pendingPromise) pendingPromisesArr.push(pendingPromise);
+    else break;
   }
 
-  return result;
+  //returning with Promise.all to resolve all promises
+  return Promise.all(pendingPromisesArr).then(() => result);
 };
 
-console.log(
-  'original --->',
-  sample.map(num => num + 10)
-);
-console.log(
-  'custom --->',
-  map(sample, num => num + 10)
-);
+(async () => {
+  const res = await bluebird.map(arr, item => item, { concurrency: 2 });
+  const res1 = await map(arr, item => item, { concurrency: 2 });
+  console.log(res, res1);
+})();
